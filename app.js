6941fdd51794e28cabaa2f81a892e46d71b48a56
Lada Koicheva <koichevalada@gmail.com>
2026-08-27 12:02:40 +0300

    refactor: streamline layout structure and enhance menu navigation
    - Remove redundant mobile wrapper div and handle image responsiveness via media query in <picture>
    - Add event handler to close burger menu automatically upon clicking navigation links

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 const menu = document.querySelector('.menu');
 const menuIcon = document.querySelector('.burger-menu');
+const mobile_nav = document.querySelector('.header-mobile-nav');
 
 
-const showMenu = () => {
+const show_hide_Menu = () => {
 
   
   menu.classList.toggle('hidden');
@@ -10,4 +11,8 @@ const showMenu = () => {
  
 }
 
-menuIcon.onclick = showMenu;
+mobile_nav.onclick = (e) => {
+  if (e.target.tagName = 'a') show_hide_Menu();
+}
+
+menuIcon.onclick = show_hide_Menu;
